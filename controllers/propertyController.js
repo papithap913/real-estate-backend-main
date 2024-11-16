b331cd6a94dc9dfb0b96f3abae010335b9303c3a
papithap913 <166851777+papithap913@users.noreply.github.com>
2024-11-17 04:42:00 +0530

    Update propertyController.js

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,4 +1,4 @@
-const Property = require("../models/propertyModel"); // Adjust path as needed
+const Property = require("../models/property"); // Adjust path as needed
 
 // Controller for adding a property
 const addProperty = async (req, res) => {
