068e75c1075750e0a9981f307aafeaec1dcea36b
papithap913 <166851777+papithap913@users.noreply.github.com>
2024-11-17 04:18:30 +0530

    Update propertyController.js

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,7 +1,3 @@
-exports.createProperty = (req, res) => {
-  // Logic for property creation, extracting data from req.body
-  res.status(201).json({ message: 'Property created successfully' });
-};
 // In propertyController.js
 exports.addProperty = async (req, res) => {
   try {
