6aebaab9617534d0b60039abb9699f3cf7feb5c3
papithap913 <166851777+papithap913@users.noreply.github.com>
2024-11-17 04:20:31 +0530

    Update propertyRoutes.js

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createProperty } = require('../controllers/propertyController'); // Adjust path as needed
-const { add-property } = require('../controllers/propertyController');
+ 
+const { addproperty } = require('../controllers/propertyController');
 router.post('/', createProperty);
-router.post("/add-property", authMiddleware, addProperty);
+router.post("/addproperty", authMiddleware, addProperty);
 
 module.exports = router;
